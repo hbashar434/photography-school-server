67121cb17e1b92b2fd02e242b6083081cbaa7368
hbashar434 <h.bashar434@gmail.com>
2023-06-10 03:11:59 +0600

    verified admin

--- index.js
+++ index.js
@@ -97,6 +97,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
     //classes routes
     app.get("/classes", async (req, res) => {
       const query = req.query?.limit;
